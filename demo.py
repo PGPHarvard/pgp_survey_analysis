266e3e9c438ae98fb65f9492288ddbee42e03e9a
Madeleine Ball <mpball@gmail.com>
2013-02-23 19:47:03 -0500

    Creating Git for PGP survey parsing code

---
+++ demo.py
@@ -0,0 +1,58 @@
+"""Demo code analyzing PGP Survey data.
+
+Prints to stdout tab-separated data. The first row is a header.
+Following lines each lists:
+ - participant huID
+ - profile URL
+ - age (from Participant Survey, if available)
+ - sex/gender (from Participant Survey, if available)
+ - race/ethnicity (from Participant Survey, if available)
+ - all possible traits (239 total as of 2013/02/23)
+
+ Only participants who responded to all trait surveys are reported.
+"""
+
+import re
+import pgp_survey
+
+SURVEYS_DIRECTORY = 'surveys_20130224'
+
+# Input trait surveys and list of traits.
+trait_surveys, trait_survey_traits = pgp_survey.get_trait_surveys(SURVEYS_DIRECTORY)
+
+# Input participant survey.
+participant_survey = pgp_survey.get_participant_survey(SURVEYS_DIRECTORY)
+
+# Use splat to get the intersection of sets returned by get_huIDs().
+in_all_surveys = trait_surveys[0].get_huIDs().intersection(
+    *[x.get_huIDs() for x in trait_surveys])
+huID_list = list(in_all_surveys)
+huID_list.sort()
+
+# Create a single list of traits from all trait surveys.
+traits_flat = []
+[traits_flat.extend(x) for x in trait_survey_traits]
+
+# Create and output header.
+header = ['huID', 'Profile URL', 'Age', 'Sex/Gender', 
+          'Race/Ethnicity'] + traits_flat
+print '\t'.join(header)
+
+# Output participant ID and 'Y' for every trait a participant has.
+for huID in huID_list:
+    url = 'https://my.personalgenomes.org/profile/' + huID
+    age = 'Unknown'
+    sex = 'Unknown'
+    race = 'Unknown'
+    if huID in participant_survey.by_huID:
+        latest_general_survey = participant_survey.get_latest_responses(huID)
+        age = latest_general_survey[1]
+        sex = latest_general_survey[13]
+        race = latest_general_survey[14]
+    huID_traits_flat = []
+    for surv_num in range(len(trait_surveys)):
+        huID_latest_data = trait_surveys[surv_num].get_latest_responses(huID)
+        traits = ['Y' if re.search(re.escape(x), huID_latest_data[1])
+                  else '' for x in trait_survey_traits[surv_num]]
+        [huID_traits_flat.append(x) for x in traits]
+    print '\t'.join([huID, url, age, sex, race] + huID_traits_flat)
---
+++ pgp_survey.py
@@ -0,0 +1,142 @@
+"""Code useful for interpreting Personal Genome Project surveys
+
+Classes:
+    SurveyData: contains survey data from PGP survey
+
+Methods:
+    get_trait_surveys: find and parse trait surveys
+    get_participant_survey: parse participant survey file
+"""
+
+import csv
+import os
+import re
+
+
+TRAITS_FILE = 'PGP_trait_list.txt'
+
+
+def _latest_matching_file(regex_string, directory='.'):
+    """Find most recently edited filename matching regex in directory."""
+    local_filenames = os.listdir(directory)
+    all_matches = []
+    for filename in local_filenames:
+        if re.match(regex_string, filename):
+            all_matches.append(os.path.join(directory, filename))
+    if all_matches:
+        return max(all_matches, key=lambda x: os.stat(x).st_mtime)
+    else:
+        return None
+
+
+class SurveyData():
+    """PGP Survey Data from Google Spreadsheets
+
+    Data attributes:
+    by_huID:       Key is huID (str), value is results (array of str).
+                    Results exclude timestamp and participant indentifiers.
+    header_titles: Header titles for results (array of str)
+    sourcefile:    Path to file that was parsed to create this object.
+
+    Methods:
+    get_huIDs:            Return huIDs that completed survey (set of str)
+    get_latest_responses: Return most recent set of survey responses.
+    get_
+
+    """
+    by_huID = dict()
+    header_titles = []
+    sourcefile = ''
+
+    def __init__(self, filepath):
+        self.by_huID = dict()
+        self.sourcefile = filepath
+        file_input = open(filepath)
+        file_data = csv.reader(file_input)
+        header_data = file_data.next()
+        self.header_titles = [header_data[1]] + header_data[3:]
+        for row in file_data:
+            huID = row[0]
+            # Assumes that the file is sorted chronologically.
+            # Data for a given huID is stored with the most recent first.
+            if huID in self.by_huID:
+                self.by_huID[huID].insert(0, [row[1]] + row[3:])
+            else:
+                self.by_huID[huID] = [[row[1]] + row[3:]]
+
+    def __repr__(self):
+        return 'SurveyData from ' + self.sourcefile
+
+    def get_huIDs(self):
+        """Returns (set): all huIDs that have completed the survey."""
+        hu_IDs = set(self.by_huID.keys())
+        return hu_IDs
+
+    def get_latest_responses(self, huID):
+        """Returns (list): most recent survey response for an huID."""
+        if huID in self.by_huID:
+            return self.by_huID[huID][0]
+        else:
+            return None
+
+    def get_responses(self, huID):
+        """Returns (list of lists): all survey responses for an huID."""
+        if huID in self.by_huID:
+            return self.by_huID[huID]
+        else:
+            return None
+
+
+def get_trait_surveys(directory='.'):
+    """Returns tuple containing trait surveys list and possible traits lists.
+
+    [0]: An array of SurveyData objects containing data from each of the 
+         trait surveys.
+    [1]: An array of arrays of str, one for each trait survey in [0].
+         Each array lists the traits covered by that survey.
+    """
+
+    # Find the most recent survey files, which should match the regexps below.
+    surveys_regexps = [
+        r'^PGPTrait&DiseaseSurvey2012-Cancers-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-Endocrine,Metabolic,Nutritional,And' +
+         'Immunity-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-Blood-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-NervousSystem-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-VisionAndHearing-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-CirculatorySystem-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-RespiratorySystem-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-DigestiveSystem-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-GenitourinarySystems-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-SkinAndSubcutaneousTissue-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-MusculoskeletalSystemAndConnective' + \
+            'Tissue-[0-9]{14}.csv$',
+        r'^PGPTrait&DiseaseSurvey2012-CongenitalTraitsAndAnomalies' + \
+            '-[0-9]{14}.csv$',
+        ]    
+    survey_files = [_latest_matching_file(x, directory) for 
+                    x in surveys_regexps]
+    assert not [x for x in survey_files if not x], \
+        "Can't find files for all trait surveys. Check that you have files " + \
+        "matching all items in this regex list: \n" + '\n'.join(surveys_regexps)
+    surveys = [SurveyData(x) for x in survey_files]
+
+    # Read the list of all possible traits, available in another text file.
+    survey_traits = [list() for x in range(len(surveys_regexps))]
+    traits_file = open(TRAITS_FILE)
+    # Find which traits are in each trait survey.
+    for line in traits_file:
+        data = line.strip().split('\t')
+        index = [x for x in range(len(surveys_regexps)) if 
+                 re.search(data[1], survey_files[x])][0]
+        survey_traits[index].append(data[0])
+
+    return (surveys, survey_traits)
+
+
+def get_participant_survey(directory='.'):
+    """Find and return the PGP participant survey (SurveyData)."""
+    survey_regex = r'PGPParticipantSurvey-[0-9]{14}.csv'
+    survey_file = _latest_matching_file(survey_regex, directory)
+    survey = SurveyData(survey_file)
+    return survey
